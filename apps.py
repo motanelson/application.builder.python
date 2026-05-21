4fd784ccb8ffa396fae939f0fbe5adb35f1c1454
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-21 19:55:15 +0100

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,80 @@
+import copy
+x=0
+heads_="""
+import tkinter as tk
+
+
+
+class myapps:
+    def __init__(self,root:tk.Tk,title:str,text:str,backgrouds:str,foregrounds:str):
+        self.root=root
+        self.root.title(title)
+        self.root.geometry("640x480")
+        self.root.configure(background=backgrouds)
+
+"""
+
+
+legs="""
+        self.control$1=tk.$2(background=backgrouds,foreground=foregrounds)
+        self.control$1.pack(padx=10,pady=10)
+"""
+def saves(files,mode,value):
+    f1=open(files,mode)
+    f1.write(value)
+    f1.close()
+
+
+def heads(files,value):
+    saves(files,"w",value)
+
+print("give me the file name .txt ? ")
+filesa=input().strip()
+
+
+def getfiles(files):
+    f1=open(files,"r")
+    values=f1.read()
+    f1.close()
+    v=values.split("\n")
+    return v
+    
+
+def defs(files,value):
+    global x
+    print("handle : function :"+value)
+    x=x+1
+    zzz=copy.copy(legs)
+    zzz=zzz.replace("$1",str(x))
+    zzz=zzz.replace("$2",value)
+    saves(files,"a",zzz)
+    
+
+print(filesa)
+gfiles=getfiles(filesa)
+
+filesa=filesa.replace(".txt",".py")
+heads(filesa,heads_)
+for n in range(len(gfiles)):
+    sss=gfiles[n].strip()
+    if sss!="":
+        defs(filesa,sss)
+
+
+
+
+
+
+
+
+heads___="""
+backgrouds="black"
+foregrounds="white"
+text=""
+title="project"
+root=tk.Tk()
+apps=myapps(root,title,text,backgrouds,foregrounds)
+root.mainloop()
+
+"""
+saves(filesa,"a",heads___)
